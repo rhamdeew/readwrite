1f70f442f8d9f0ec52253504da6e1d10059552a3
Раиль Хамдеев <rail@MacBook-Air-Rail.local>
2019-01-10 02:53:35 +0400

    sync verision beta

--- main.py
+++ main.py
@@ -4,34 +4,34 @@ import random
 import redis
 from time import sleep
 
-def id_generator(size=6, chars=string.ascii_uppercase + string.digits):
+def random_msg(size=6, chars=string.ascii_uppercase + string.digits):
     return ''.join(random.choice(chars) for _ in range(size))
 
 def main():
-    process_id = uuid.uuid1()
+    process_id = str(uuid.uuid1())
     r = redis.StrictRedis(host='localhost', port=6379, db=2)
 
     mode = 1 # 0 - reader, 1 - writer
-    if r.get('generator'):
+    current_generator = r.get('generator')
+    if current_generator and current_generator != process_id:
         mode = 0
     else:
-        r.set('generator', str(process_id))
-        r.expire('generator', 10)
+        r.set('generator', process_id, 10)
         mode = 1
 
     while True:
         if mode is 1:
-            gen = id_generator()
-            r.rpush('messages', gen)
-            print('Send: '+ gen)
+            r.set('generator', process_id, 10)
+            msg = random_msg()
+            r.rpush('messages', msg)
+            print('Send: '+ msg)
         else:
             msg = r.rpop('messages')
             if msg is not None:
                 print('Receive: ' + str(msg))
             else:
                 if r.get('generator') is None:
-                    r.set('generator', str(process_id))
-                    r.expire('generator', 10)
+                    r.set('generator', process_id, 10)
                     mode = 1
         sleep(1)
 
