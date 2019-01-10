81232b3735270c93f036360d868a884e9366fa4a
Раиль Хамдеев <rail@MacBook-Air-Rail.local>
2019-01-10 04:10:44 +0400

    with async final

--- async.py
+++ async.py
@@ -3,15 +3,13 @@ import uuid
 import string
 import random
 import redis
+import sys
 from time import sleep
 
 def random_msg(size=6, chars=string.ascii_uppercase + string.digits):
     return ''.join(random.choice(chars) for _ in range(size))
 
-async def work():
-    process_id = str(uuid.uuid1())
-    r = redis.StrictRedis(host='localhost', port=6379, db=2)
-
+async def work(r, process_id):
     mode = 1 # 0 - reader, 1 - writer
     current_generator = r.get('generator')
     if current_generator and current_generator != process_id:
@@ -21,7 +19,7 @@ async def work():
         mode = 1
 
     while True:
-        if mode is 1:
+        if mode == 1:
             r.set('generator', process_id, 10)
             msg = random_msg()
             r.rpush('messages', msg)
@@ -30,15 +28,27 @@ async def work():
             msg = r.rpop('messages')
             if msg is not None:
                 print('Receive: ' + str(msg))
+                random_int = random.randint(1, 20)
+                if random_int == 1:
+                    r.rpush('errors', msg)
             else:
                 if r.get('generator') is None:
                     r.set('generator', process_id, 10)
                     mode = 1
         await asyncio.sleep(1)
 
+
+process_id = str(uuid.uuid1())
+r = redis.StrictRedis(host='localhost', port=6379, db=2)
+
+if len(sys.argv)>1 and str(sys.argv[1]) == 'getErrors':
+    print(r.lrange('errors', 0, -1))
+    r.delete('errors')
+    sys.exit()
+
 loop = asyncio.get_event_loop()
 try:
-    asyncio.ensure_future(work())
+    asyncio.ensure_future(work(r, process_id))
     loop.run_forever()
 except KeyboardInterrupt:
     pass
--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import uuid
 import string
 import random
 import redis
+import sys
 from time import sleep
 
 def random_msg(size=6, chars=string.ascii_uppercase + string.digits):
@@ -10,6 +11,11 @@ def random_msg(size=6, chars=string.ascii_uppercase + string.digits):
 def main():
     process_id = str(uuid.uuid1())
     r = redis.StrictRedis(host='localhost', port=6379, db=2)
+
+    if len(sys.argv)>1 and str(sys.argv[1]) == 'getErrors':
+        print(r.lrange('errors', 0, -1))
+        r.delete('errors')
+        sys.exit()
 
     mode = 1 # 0 - reader, 1 - writer
     current_generator = r.get('generator')
@@ -20,7 +26,7 @@ def main():
         mode = 1
 
     while True:
-        if mode is 1:
+        if mode == 1:
             r.set('generator', process_id, 10)
             msg = random_msg()
             r.rpush('messages', msg)
@@ -29,6 +35,9 @@ def main():
             msg = r.rpop('messages')
             if msg is not None:
                 print('Receive: ' + str(msg))
+                random_int = random.randint(1, 20)
+                if random_int == 1:
+                    r.rpush('errors', msg)
             else:
                 if r.get('generator') is None:
                     r.set('generator', process_id, 10)
